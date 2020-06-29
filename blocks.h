adb70e7c48bf0ef32cd6ce8cd1e2200fadabb6ee
lordrusk <lordruskk@gmail.com>
2020-06-29 17:17:50 +0000

    added new module

--- blocks.h
+++ blocks.h
@@ -5,6 +5,7 @@ static const Block blocks[] = {
 	{"",		"music",			0,			11},
 	{"",		"pacpackages",			0,			8},
 	{"",		"news",				0,			6},
+	{"",		"georona",			18000,			19},
 	{"",		"crypto",			18000,			17},
 	{"",		"NULL",				0,			0},
 	{"",		"torrent",			20,			7},
@@ -45,4 +46,5 @@ static char delim = ' ';
  * 16 nettraf
  * 17 crypto
  * 18 astrological
+ * 19 georona
  */
