e750ace14b608a22dc964946b06d36d26ba6f7f3
lordrusk <lordruskk@gmail.com>
2020-06-30 18:49:04 +0000

    no need for NULL script, delim can now be string, and other imporovements

--- blocks.h
+++ blocks.h
@@ -24,7 +24,7 @@ static const Block blocks[] = {
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = ' ';
+static char *delim = " ";
 
 /* Quick list of modules Update Signals
  * 0 NULL
